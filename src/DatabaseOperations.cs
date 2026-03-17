11774ecc3c1c5861855fdeff9e7c19a863b0f1e9
JaredMSFT <jaredmeade@microsoft.com>
2026-03-17 17:19:21 -0400

    bring your own data source

--- src/DatabaseOperations.cs
+++ src/DatabaseOperations.cs
@@ -25,14 +25,16 @@ internal sealed class DatabaseOperations : IDatabaseOperations, IAsyncDisposable
     private readonly object ddlLock = new object();
 
     private readonly NpgsqlDataSource ds;
+    private readonly bool isExternalDataSource;
 
-    public DatabaseOperations(NpgsqlDataSource dataSource, string schemaName, string tableName, bool useWAL, bool createIfNotExists, TimeProvider timeProvider) {
+    public DatabaseOperations(NpgsqlDataSource dataSource, string schemaName, string tableName, bool useWAL, bool createIfNotExists, TimeProvider timeProvider, bool isExternalDataSource = false) {
         ds = dataSource ?? throw new ArgumentNullException(nameof(dataSource));
         SchemaName = schemaName;
         TableName = tableName;
         UseWAL = useWAL;
         CreateIfNotExists = createIfNotExists;
         TimeProvider = timeProvider;
+        this.isExternalDataSource = isExternalDataSource;
         SqlQueries = new SqlQueries(schemaName, tableName, useWAL);
     }
 
@@ -356,6 +358,6 @@ internal sealed class DatabaseOperations : IDatabaseOperations, IAsyncDisposable
     }
 
     public ValueTask DisposeAsync() {
-        return ds.DisposeAsync();
+        return isExternalDataSource ? default : ds.DisposeAsync();
     }
 }
